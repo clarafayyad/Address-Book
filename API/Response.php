a72069a5c3b78279e18bda64150920a0cf9f01ae
Clara Fayyad <clara.fayad.intern@anghami.com>
2021-02-03 07:46:22 +0200

    modified Response.php

--- API/Response.php
+++ API/Response.php
@@ -1,5 +1,6 @@
 <?php
 
+//function used by all endpoints to return a response message with a proper http response code
 function returnMessage($message, $code){
     echo json_encode($message);
     http_response_code($code);
